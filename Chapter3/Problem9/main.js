a381169c5d970f24b76218a38bb98a7ea74dcf42
Paul Adolph <padolph@gmail.com>
2018-09-12 15:53:09 -0700

    Fixed bugs in C03P09 RC4 javascript implementation, now matches wikipedia test vectors

--- Chapter3/Problem9/main.js
+++ Chapter3/Problem9/main.js
@@ -1,18 +1,35 @@
-// TODO: run actual encrypt and use test vectors to check
+// TODO: run actual encrypt and use test vectors to check,
+// like https://en.wikipedia.org/wiki/RC4#Test_vectors
 
-var key = new Buffer([0x1a, 0x2b, 0x3c, 0x4d, 0x5e, 0x6f, 0x77]),
+var key = Buffer.from([0x1a, 0x2b, 0x3c, 0x4d, 0x5e, 0x6f, 0x77]),
+//var key = new Buffer([0x4b, 0x65, 0x79]),  // "Key"
+//var key = new Buffer([0x57, 0x69, 0x6b, 0x69]),  // "Wiki"
     N = key.length,
-    S = new Buffer(256),
-    K = new Buffer(256),
+    S = Buffer.allocUnsafe(256),
+    K = Buffer.allocUnsafe(256),
     i = 0,
     j = 0;
 
 initS();
+
+/*
+for (var c = 0; c < 10; c++) {
+    var byte = getKeystreamByte();
+    console.log(byteToHex(byte));
+}
+*/
+
 console.log('Initial S:');
 printS();
 
-console.log('\nDiscarding 100 keystream bytes');
-discardKeystreamBytes(100);
+console.log('\nDiscarding 1 keystream byte');
+discardKeystreamBytes(1);
+printIndeces();
+console.log('S after the first 1 keystream byte generated:');
+printS();
+
+console.log('\nDiscarding 99 keystream bytes');
+discardKeystreamBytes(99);
 printIndeces();
 console.log('S after the first 100 keystream bytes generated:');
 printS();
@@ -28,28 +45,28 @@ function printIndeces() {
 }
 
 function discardKeystreamBytes(n) {
-    var i;
-    for (i = 0; i < 100; i++) {
+    var k;
+    for (k = 0; k < n; k++) {
         getKeystreamByte();
     }
 }
 
 function initS() {
-    var i, j=0;
-    for (i=0; i < 256; i++) {
-        S[i] = i;
-        K[i] = key[i % N];
+    var k, m=0;
+    for (k=0; k < 256; k++) {
+        S[k] = k;
+        K[k] = key[k % N];
     }
-    for (i=0; i < 256; i++) {
-        j = (j + S[i] + K[i]) % 256;
-        swapS(i, j);
+    for (k=0; k < 256; k++) {
+        m = (((m + S[k]) % 256) + K[k]) % 256;
+        swapS(k, m);
     }
 }
 
-function swapS(i, j) {
-    var tmp = S[i];
-    S[i] = S[j];
-    S[j] = tmp;
+function swapS(a, b) {
+    var tmp = S[a];
+    S[a] = S[b];
+    S[b] = tmp;
 }
 
 function printS() {
@@ -76,7 +93,7 @@ function byteToHex(d, padding) {
 function getKeystreamByte() {
     var t;
     // NOTE: using global i, j
-    i = (i + 1) % 256;
+     i = (i + 1) % 256;
     j = (j + S[i]) % 256;
     swapS(i, j);
     t = (S[i] + S[j]) % 256
